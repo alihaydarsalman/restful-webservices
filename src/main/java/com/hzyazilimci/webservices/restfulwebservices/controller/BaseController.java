7ed990cc0ce6907df297d26222533e5dbe1d7bc7
Alihaydar SALMAN <1030522871@erciyes.edu.tr>
2023-06-05 23:04:44 +0300

    implementing i18n with resource bundles

--- src/main/java/com/hzyazilimci/webservices/restfulwebservices/controller/BaseController.java
+++ src/main/java/com/hzyazilimci/webservices/restfulwebservices/controller/BaseController.java
@@ -1,17 +1,34 @@
 package com.hzyazilimci.webservices.restfulwebservices.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 /**
  * @author hzyazilimci
  */
+
+@RequiredArgsConstructor
 @RestController
 public class BaseController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/api/welcome")
+    private final MessageSource messageSource;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/welcome")
     public String welcome(){
         return "Welcome";
+    }
+
+    @GetMapping("/greetingWithI18N")
+    public String greetingWithI18N(){
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "DEFAULT MESSAGE FOR THIS METHOD", locale);
     }
 }
